d9cc1d4fff7486f02c9ff11d364be53e938a6b9d
yaumil <yaumilramkhadani@gmail.com>
2025-03-04 22:30:25 +0700

    optimize code

--- lib/Presentations/dashboard/widgets/dashboard_card.dart
+++ lib/Presentations/dashboard/widgets/dashboard_card.dart
@@ -4,12 +4,6 @@ import 'package:SummitDocs/commons/widgets/app_text.dart';
 import 'package:SummitDocs/core/config/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
-import 'package:SummitDocs/Presentations/dashboard/widgets/status_file.dart';
-import 'package:SummitDocs/commons/widgets/app_button.dart';
-import 'package:SummitDocs/commons/widgets/app_text.dart';
-import 'package:SummitDocs/core/config/theme/app_colors.dart';
-import 'package:flutter/material.dart';
-
 class DashboardCard extends StatefulWidget {
   final String title;
   final List<HistoryItem> historyItems;
@@ -37,56 +31,48 @@ class _DashboardCardState extends State<DashboardCard> {
 
   void _toggleList() {
     setState(() {
-      if (itemsToShow >= widget.historyItems.length) {
-        itemsToShow = widget.initialItemsToShow; // Reset ke jumlah awal
-      } else {
-        itemsToShow = (itemsToShow + 5).clamp(0, widget.historyItems.length);
-      }
+      itemsToShow = itemsToShow >= widget.historyItems.length
+          ? widget.initialItemsToShow
+          : (itemsToShow + 5).clamp(0, widget.historyItems.length);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: double.infinity,
       decoration: BoxDecoration(
         color: AppColors.background,
         border: Border.all(
           color: AppColors.grayBackground,
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AppText(
-              text: widget.title,
-              fontWeight: FontWeight.w700,
-              fontSize: 16,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText(
+            text: widget.title,
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+          ),
+          const SizedBox(height: 12),
+          ...List.generate(
+            itemsToShow,
+            (index) => HistoryItemTile(item: widget.historyItems[index]),
+          ),
+          const SizedBox(height: 8),
+          if (widget.historyItems.length > widget.initialItemsToShow)
+            AppButton(
+              action: _toggleList,
+              text: itemsToShow >= widget.historyItems.length
+                  ? "Show Less"
+                  : "View More",
+              borderColor: AppColors.grayBackground2,
+              backgroundColor: AppColors.secondaryBackground,
+              fontColor: Colors.black,
             ),
-            const SizedBox(height: 12),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: itemsToShow,
-              itemBuilder: (context, index) {
-                return HistoryItemTile(item: widget.historyItems[index]);
-              },
-            ),
-            const SizedBox(height: 8),
-            if (widget.historyItems.length > widget.initialItemsToShow)
-              AppButton(
-                action: _toggleList,
-                text: itemsToShow >= widget.historyItems.length
-                    ? "Show Less"
-                    : "View More",
-                borderColor: AppColors.grayBackground2,
-                backgroundColor: AppColors.secondaryBackground,
-                fontColor: Colors.black,
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
@@ -97,7 +83,7 @@ class HistoryItem {
   final String statusText;
   final bool status;
 
-  HistoryItem({
+  const HistoryItem({
     required this.text,
     required this.statusText,
     required this.status,
