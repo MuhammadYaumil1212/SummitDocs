e9b822146da4dafcb4fe9b90544824bdc01ac5a3
yaumil <yaumilramkhadani@gmail.com>
2025-03-04 23:17:21 +0700

    fix width

--- lib/Presentations/dashboard/widgets/status_file.dart
+++ lib/Presentations/dashboard/widgets/status_file.dart
@@ -7,6 +7,7 @@ import 'package:flutter_svg/svg.dart';
 class StatusFile extends StatelessWidget {
   final String statusText;
   final bool status;
+
   const StatusFile({
     super.key,
     required this.statusText,
@@ -15,28 +16,28 @@ class StatusFile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Center(
-            child: SvgPicture.asset(
-              status ? AppString.icSuccess : AppString.icfailed,
-              width: 16,
-              height: 16,
-            ),
-          ),
-          SizedBox(width: !status ? 9 : 4),
-          AppText(
-            text: statusText,
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-            fontColor: status ? AppColors.greenSuccess : AppColors.redFailed,
-          ),
-        ],
-      ),
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          status ? AppString.icSuccess : AppString.icfailed,
+          width: 16,
+          height: 16,
+        ),
+        const SizedBox(width: 8),
+        AppText(
+          text: statusText,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          fontColor: status ? AppColors.greenSuccess : AppColors.redFailed,
+        ),
+        !status
+            ? const SizedBox(width: 5.7)
+            : const SizedBox(
+                width: 0,
+              )
+      ],
     );
   }
 }
