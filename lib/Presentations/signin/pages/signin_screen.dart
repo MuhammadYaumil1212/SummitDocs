5bba3217cb0f365462417c1214ac2f4df7fb9afb
yaumil <yaumilramkhadani@gmail.com>
2025-03-08 00:37:08 +0700

    slicing login

--- lib/Presentations/signin/pages/signin_screen.dart
+++ lib/Presentations/signin/pages/signin_screen.dart
@@ -1,10 +1,13 @@
 import 'package:SummitDocs/Presentations/signin/widgets/login_card.dart';
+import 'package:SummitDocs/commons/constants/string.dart';
 import 'package:SummitDocs/commons/widgets/app_button.dart';
 import 'package:SummitDocs/core/helper/message/message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../commons/widgets/app_scaffold.dart';
 import '../../../commons/widgets/app_text.dart';
+import '../../../core/config/theme/app_colors.dart';
 import '../bloc/signin_bloc.dart';
 
 class SigninScreen extends StatefulWidget {
@@ -35,6 +38,23 @@ class _SigninScreenState extends State<SigninScreen> {
           return DisplayMessage.errorMessage(state.message, context);
         }
       },
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        title: SvgPicture.asset(
+          AppString.logoApp,
+          width: 45,
+          height: 45,
+        ),
+        backgroundColor: Colors.white,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: AppColors.grayBackground,
+            height: 4.0,
+          ),
+        ),
+      ),
 
       /// Use bloc builder if you wanna used the state
       appWidget: Column(
