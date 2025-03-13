945f7b7e5d7df249eff50ded30e450e75762b8d5
yaumil <yaumilramkhadani@gmail.com>
2025-03-13 12:40:26 +0700

    add dropdown

--- lib/commons/widgets/app_textfield.dart
+++ lib/commons/widgets/app_textfield.dart
@@ -1,3 +1,4 @@
+import 'package:SummitDocs/commons/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 class AppTextfield extends StatefulWidget {
@@ -89,6 +90,85 @@ class _AppTextfieldState extends State<AppTextfield> {
                   },
                 )
               : null,
+        ),
+      ),
+    );
+  }
+}
+
+class AppDropdown extends StatefulWidget {
+  final String label;
+  final List<String> items;
+  final ValueChanged<String?> onChanged;
+
+  const AppDropdown({
+    Key? key,
+    required this.label,
+    required this.items,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  _AppDropdownState createState() => _AppDropdownState();
+}
+
+class _AppDropdownState extends State<AppDropdown> {
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: DropdownButtonFormField<String>(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintStyle: const TextStyle(color: Colors.grey),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey, width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            errorBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            focusedErrorBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red, width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+          ),
+          hint: AppText(
+            text: widget.label,
+            fontColor: Colors.grey,
+          ),
+          value: selectedValue,
+          items: widget.items
+              .map((value) => DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  ))
+              .toList(),
+          onChanged: (newValue) {
+            setState(() {
+              selectedValue = newValue;
+            });
+            widget.onChanged(newValue);
+          },
         ),
       ),
     );
